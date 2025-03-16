10e30e14f3927c95920f05422546842e1a864595
abhay2602 <97271757+abhay2602@users.noreply.github.com>
2025-03-16 22:52:43 +0530

    temp local host

--- index.js
+++ index.js
@@ -9,21 +9,31 @@ dotenv.config();
 
 // Middleware to parse JSON
 app.use(express.json());
-app.use(cors());
+
+// CORS Configuration (Only Local for Now)
+const allowedOrigins = [
+  "http://localhost:5173", // Local frontend for testing
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 const connect = async () => {
-  try{
+  try {
     await mongoose.connect(process.env.MONGO);
-    console.log("connected to mongodb");
-  }
-  catch(error){
+    console.log("Connected to MongoDB");
+  } catch (error) {
     throw error;
   }
 };
 
 app.use("/api/rout", loanRoute);
 
-app.listen(8800, ()=>{
-  connect()
-  console.log("connected to backend");
-})
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend");
+});
